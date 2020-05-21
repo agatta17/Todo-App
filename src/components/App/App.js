b7f51ca010d78ebbe7a7ebc2f5c6a339c330bec4
Anastasiya Ephimova <ad.ephimova@gmail.com>
2020-05-21 16:36:26 +0400

    classnames

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,9 +5,15 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 const items = [
-	{value: 'Сделать урок по React'},
-	{value: 'Приготовить обед'},
-	{value: 'Помыть посуду'}
+	{value: 'Сделать урок по React',
+	isDone: true
+},
+	{value: 'Приготовить обед',
+	isDone: true
+},
+	{value: 'Помыть посуду',
+	isDone: false
+}
 ];
 
 const App = () => (<div className={styles.wrap}>
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Item from '../Item/Item';
 
 const ItemList = ({items}) => (<ul>
-{items.map(item => <li key={item.value}><Item value = {item.value}/></li>)}
+  {items.map(item => <li key={item.value}>
+	<Item value = {item.value} isDone = {item.isDone}/>
+  </li>)}
 </ul>);
 
 export default ItemList;
