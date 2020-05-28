59bd6ea92a833b98bd735f145ad526051bcdec39
Anastasiya Ephimova <ad.ephimova@gmail.com>
2020-05-28 12:13:39 +0400

    design

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Item from '../Item/Item';
+import styles from './ItemList.module.css';
 
-const ItemList = ({items}) => (<ul>
+  const ItemList = ({items}) => (<ul className={styles.itemlist}>
   {items.map(item => <li key={item.value}>
 	<Item value = {item.value} isDone = {item.isDone}/>
   </li>)}
