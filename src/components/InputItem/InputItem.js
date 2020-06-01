bbf2da9b19084e02386f4ae0e19f2502cd6ac289
Anastasiya Ephimova <ad.ephimova@gmail.com>
2020-06-01 15:36:32 +0400

    code refactoring of App.js

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-// import { styled } from '@material-ui/core/styles';
 
-const InputItem = () => (<div>
-    <TextField
-        label="Добавить задание"
-        id="standard-basic" 
-    />
-</div>);
+class InputItem extends React.Component {
+  render() {
+    return (
+      <div><TextField label="Добавить задание" id="standard-basic"  /></div>);
+  }
+}
 
 export default InputItem;
