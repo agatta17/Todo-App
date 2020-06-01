c1337202ab8da147c8d3a7f3a88dc6400a5bcb9a
Anastasiya Ephimova <ad.ephimova@gmail.com>
2020-06-01 16:49:38 +0400

    add event handler

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,12 +26,16 @@ class App extends React.Component {
     ]
   };
 
+  onClickDone = isDone => {
+    console.log(isDone);
+  }
+
   render() {
   	return (
       <div className={styles.wrap}>
         <h1 className={styles.title}>Задачи на сегодня:</h1>
         <InputItem />
-        <ItemList items = {this.state.items}/>
+        <ItemList items={this.state.items} onClickDone={this.onClickDone} />
         <Footer count={2} />
       </div>);
   }
