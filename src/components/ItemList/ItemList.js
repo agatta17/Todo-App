4f35c394322d2c262bdbaf2df63e9b7e11611c4d
Anastasiya Ephimova <ad.ephimova@gmail.com>
2020-06-02 16:21:26 +0400

    add prop-types

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
+import PropTypes from 'prop-types';
 
 const ItemList = ({items, onClickDone, onClickDelete}) => (<ul className={styles.itemlist}>
   {items.map(item => <li key={item.id}>
@@ -13,5 +14,11 @@ const ItemList = ({items, onClickDone, onClickDelete}) => (<ul className={styles
 		/>
   </li>)}
 </ul>);
+
+ItemList.propTypes = {
+  items: PropTypes.array.isRequired,
+  onClickDone: PropTypes.func.isRequired,
+  onClickDelete: PropTypes.func.isRequired,
+};
 
 export default ItemList;
