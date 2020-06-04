76dbcbfa8ac7b94a98656fba0ad2c334b5fedec6
Anastasiya Ephimova <ad.ephimova@gmail.com>
2020-06-04 18:56:30 +0400

    install react-router-dom, add menu

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './About.module.css';
+
+
+function About() {
+	return (
+		<h1 className={styles.title}>Обо мне</h1>);
+};
+
+export default About;
+
+
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,71 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import InputItem from '../InputItem/InputItem';
-import ItemList from '../ItemList/ItemList';
-import Footer from '../Footer/Footer';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Todo from '../Todo/Todo';
+import About from '../About/About';
+import Contacts from '../Contacts/Contacts';
 import styles from './App.module.css';
+import Card from '@material-ui/core/Card';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import AppBar from '@material-ui/core/AppBar';
 
 function App() {
-  useEffect(() => {console.log('update')});
-  useEffect(() => {console.log('mount')},[]);
-  const [count, setCount] = useState(4);
-  const [items, setItems] = useState(
-    [
-      {
-        value: 'Сделать урок по React',
-        isDone: false,
-        id: 1
-      },
-      {
-        value: 'Приготовить обед',
-        isDone: false,
-        id: 2
-      },
-      {
-        value: 'Помыть посуду',
-        isDone: false,
-        id: 3
-      },
-      {
-        value: 'Погулять с собакой',
-        isDone: false,
-        id: 4
-      }
-    ]);
 
-  const onClickDone = id => {
-    const newItemList = items.map(item => {
-      const newItem = { ...item };
-      if (item.id === id) {
-        newItem.isDone = !item.isDone;
-      }
-      return newItem;
-    });
-    setItems(newItemList);
-    };
-  
-  const onClickDelete = id => {
-    const newItemList = items.filter(item => item.id !== id);
-    setItems(newItemList);
-    };
-
-  const onClickAdd = value => {
-      setItems(
-        [...items, {
-          value,
-          isDone: false,
-          id: count + 1
-          }
-        ]);
-      setCount(count + 1)
-  };
-  
 	return (
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>Задачи на сегодня:</h1>
-      <InputItem onClickAdd={onClickAdd} />
-      <ItemList items={items} onClickDone={onClickDone} onClickDelete={onClickDelete} />
-      <Footer count={count} />
-    </div>);
+    <Router>
+      <div className={styles.wrap}>
+        <AppBar position='static' color='transparent'>
+          <MenuList className={styles.menu} id="simple-menu">
+            <Link className={styles.link} to='/'><MenuItem>Обо мне</MenuItem></Link>
+            <Link className={styles.link} to='/todo'><MenuItem>Задачи</MenuItem></Link>
+            <Link className={styles.link} to='/contacts'><MenuItem>Контакты</MenuItem></Link>
+          </MenuList>
+        </AppBar>
+        <Card className={styles.content}>
+          <Route path='/' exact component={About} />
+          <Route path='/todo' component={Todo} />
+          <Route path='/contacts' component={Contacts} />
+        </Card>
+      </div>
+    </Router>);
   
 };
 
