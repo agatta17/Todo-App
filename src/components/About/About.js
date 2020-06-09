98f26f04ddca4eaae81795ee1fe1f5e7619a766f
Anastasiya Ephimova <ad.ephimova@gmail.com>
2020-06-09 20:46:23 +0400

    add about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,100 @@
 import React from 'react';
 import styles from './About.module.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {Octokit} from '@octokit/rest';
+
+const octokit = new Octokit();
+
+class About extends React.Component {
+	state = {
+		isLoading: true,
+		fetchReposFailure: false,
+		repoList: [],
+		fetchUserFailure: false,
+		userInfo: {}
+	}
+	componentDidMount() {
+		octokit.repos.listForUser({
+  		username: 'agatta17'
+		}).then(
+    successResponse => {
+      if (successResponse.status != 200) {
+        this.setState({
+					fetchReposFailure: true,
+					isLoading: false
+				})
+      } 
+      else {
+				this.setState({
+					repoList: successResponse.data,
+					isLoading: false,
+					fetchReposFailure: false,
+				});
+      }
+    },
+    failResponse => {
+      this.setState({
+				fetchReposFailure: true,
+				isLoading: false
+			});
+    })
+
+    octokit.users.getByUsername({
+		  username: 'agatta17'
+		}).then(
+    successResponse => {
+      if (successResponse.status != 200) {
+        this.setState({
+					fetchUserFailure: true,
+					isLoading: false
+				})
+      } 
+      else {
+				this.setState({
+					userInfo: successResponse.data,
+					isLoading: false,
+					fetchUserFailure: false,
+				});
+      }
+    },
+    failResponse => {
+      this.setState({
+				fetchUserFailure: true,
+				isLoading: false
+			});
+    })
+	};
 
 
-function About() {
-	return (
-		<h1 className={styles.title}>Обо мне</h1>);
-};
+
+	render() {
+		const {isLoading, fetchReposFailure, repoList, fetchUserFailure, userInfo} = this.state;
+		return (
+			<>
+				{isLoading ? <CircularProgress color="secondary" className={styles.progress}/> : <h1 className={styles.title}>Обо мне</h1>}
+
+				{!isLoading && !fetchUserFailure && <div>
+					<img src={userInfo.avatar_url} className={styles.avatar}/>
+					<p>{userInfo.name}</p>
+					<p>{userInfo.bio}</p>
+					<p><a href={userInfo.html_url} target='blank'>Аккаунт на GitHub</a></p>
+				</div>}
+
+				{fetchUserFailure && <div className={styles.error}>Ошибка запроса: данные о пользователе не найдены!</div>}
+
+				<p>Список репозиториев:</p>
+				{!isLoading && !fetchReposFailure && <ul>
+						{repoList.map(repo => (<li key={repo.id}>
+								<a href={repo.html_url} target='blank'>{repo.name}</a>
+						</li>))}
+					</ul>}
+
+				{fetchReposFailure && <div className={styles.error}>Ошибка запроса: репозитории пользователя не найдены!</div>}
+
+			</>
+		);
+	}
+}
 
 export default About;
 
