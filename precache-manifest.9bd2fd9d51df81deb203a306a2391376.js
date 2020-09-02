322a88c713955836a49366cb5f389db909a2cd75
Anastasiya Ephimova <ad.ephimova@gmail.com>
2020-09-02 14:36:14 +0400

    Updates

---
+++ precache-manifest.9bd2fd9d51df81deb203a306a2391376.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f9a15dbe0342170d461c7340b91aab",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "2a1fdf47f2ffe046e5a8",
+    "url": "/Todo-App/static/css/main.a8686899.chunk.css"
+  },
+  {
+    "revision": "740e8160ab6111b2c118",
+    "url": "/Todo-App/static/js/2.0a64c397.chunk.js"
+  },
+  {
+    "revision": "ea400bfb7cfb62de59f10ac82e4a6f9d",
+    "url": "/Todo-App/static/js/2.0a64c397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1fdf47f2ffe046e5a8",
+    "url": "/Todo-App/static/js/main.003d4e1e.chunk.js"
+  },
+  {
+    "revision": "37ecbcee180bd499abdd",
+    "url": "/Todo-App/static/js/runtime-main.e5872ecb.js"
+  },
+  {
+    "revision": "b3005827ed31f1a4eeaa1258713f0990",
+    "url": "/Todo-App/static/media/gmail.b3005827.svg"
+  },
+  {
+    "revision": "3322efd5b6b9556c0551505ad89374d1",
+    "url": "/Todo-App/static/media/icon-attention.3322efd5.svg"
+  },
+  {
+    "revision": "160b0c9999b9439bfa06d8d977f3a2ef",
+    "url": "/Todo-App/static/media/instagram.160b0c99.svg"
+  },
+  {
+    "revision": "bdb72f003b910e6d1fefecc8bbb422b2",
+    "url": "/Todo-App/static/media/no-items.bdb72f00.png"
+  },
+  {
+    "revision": "f2ba364a38137d4512bddc8be29a19c0",
+    "url": "/Todo-App/static/media/pencil.f2ba364a.svg"
+  },
+  {
+    "revision": "145db91c635776ea135f1e7e64098732",
+    "url": "/Todo-App/static/media/remove.145db91c.svg"
+  },
+  {
+    "revision": "a888fec0a2859e59674a89bc7722e062",
+    "url": "/Todo-App/static/media/telegram.a888fec0.svg"
+  },
+  {
+    "revision": "b03024c4d635b0a304e483e1d9fa34d8",
+    "url": "/Todo-App/static/media/vk.b03024c4.svg"
+  }
+]);
